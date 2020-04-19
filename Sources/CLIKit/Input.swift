07e59fcc7dbcda757c79ed222de05e30dd68f905
natgen <62327702+natgen@users.noreply.github.com>
2020-04-19 15:17:45 +0930

    Create readString Function

--- Sources/CLIKit/Input.swift
+++ Sources/CLIKit/Input.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
+public func readString(repeatOnFailure: Bool? = false, executeOnFailure: () -> Void) -> String? {
+    
+    if let repeatOnFailure = repeatOnFailure {
+        
+        if repeatOnFailure {
+            
+            while true {
+                if let input = readLine() {
+                    if let string: String = String(input) {
+                        return string
+                    }
+                    executeOnFailure()
+                }
+            }
+            
+        } else {
+            
+            if let input = readLine() {
+                if let string: String = String(input) {
+                    return string
+                }
+                executeOnFailure()
+            }
+            
+        }
+        
+    }
+    
+    return nil
+    
+}
+
 public func readInt(repeatOnFailure: Bool? = false, executeOnFailure: () -> Void) -> Int? {
     
     if let repeatOnFailure = repeatOnFailure {
@@ -18,8 +50,8 @@ public func readInt(repeatOnFailure: Bool? = false, executeOnFailure: () -> Void
                     if let int: Int = Int(input) {
                         return int
                     }
+                    executeOnFailure()
                 }
-                executeOnFailure()
             }
             
         } else {
